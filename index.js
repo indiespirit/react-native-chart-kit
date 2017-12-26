63e8fe19175212579df93af38af75ce816688c8c
Herman Starikov <herman.starikov@uxpsystems.com>
2017-12-26 15:43:36 -0500

    npm prep

---
+++ index.js
@@ -0,0 +1,15 @@
+import AbstractChart from './src/abstract-chart'
+import LineChart from './src/line-chart'
+import BarChart from './src/bar-chart'
+import PieChart from './src/pie-chart'
+import ProgressChart from './src/progress-chart'
+import ContributionGraph from './src/contribution-graph'
+
+export {
+  AbstractChart,
+  BarChart,
+  LineChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph
+}
