078b5ff23ff0608839a828e57b713ffd1f42c829
Herman Starikov <hermanstarikov@gmail.com>
2019-03-22 21:42:23 -0400

    minor fixes

--- src/bar-chart.js
+++ src/bar-chart.js
@@ -1,63 +1,68 @@
 import React from 'react'
-import { View } from 'react-native'
-import {
-  Svg,
-  Rect,
-  G
-} from 'react-native-svg'
+import {View} from 'react-native'
+import {Svg, Rect, G} from 'react-native-svg'
 import AbstractChart from './abstract-chart'
 
 const barWidth = 32
 
 class BarChart extends AbstractChart {
   renderBars = config => {
-    const { data, width, height, paddingTop, paddingRight } = config
+    const {data, width, height, paddingTop, paddingRight} = config
     return data.map((x, i) => {
-      const barHeight = height / 4 * 3 * ((x - Math.min(...data)) / this.calcScaler(data))
+      const barHeight =
+        (height / 4) * 3 * ((x - Math.min(...data)) / this.calcScaler(data))
       const barWidth = 32
       return (
         <Rect
           key={Math.random()}
-          x={(paddingRight + (i * (width - paddingRight) / data.length) + (barWidth / 2))}
-          y={(((height / 4 * 3) - barHeight) + paddingTop)}
+          x={
+            paddingRight +
+            (i * (width - paddingRight)) / data.length +
+            barWidth / 2
+          }
+          y={(height / 4) * 3 - barHeight + paddingTop}
           width={barWidth}
           height={barHeight}
           fill="url(#fillShadowGradient)"
-        />)
+        />
+      )
     })
   }
 
   renderBarTops = config => {
-    const { data, width, height, paddingTop, paddingRight } = config
+    const {data, width, height, paddingTop, paddingRight} = config
     return data.map((x, i) => {
-      const barHeight = height / 4 * 3 * ((x - Math.min(...data)) / this.calcScaler(data))
+      const barHeight =
+        (height / 4) * 3 * ((x - Math.min(...data)) / this.calcScaler(data))
       return (
         <Rect
           key={Math.random()}
-          x={(paddingRight + (i * (width - paddingRight) / data.length)) + (barWidth / 2)}
-          y={(((height / 4 * 3) - barHeight) + paddingTop)}
+          x={
+            paddingRight +
+            (i * (width - paddingRight)) / data.length +
+            barWidth / 2
+          }
+          y={(height / 4) * 3 - barHeight + paddingTop}
           width={barWidth}
           height={2}
           fill={this.props.chartConfig.color(0.6)}
-        />)
+        />
+      )
     })
   }
 
   render() {
     const paddingTop = 16
     const paddingRight = 64
-    const { width, height, data, style = {} } = this.props
-    const { borderRadius = 0 } = style
+    const {width, height, data, style = {}} = this.props
+    const {borderRadius = 0} = style
     const config = {
       width,
       height
     }
     return (
       <View style={style}>
-        <Svg
-          height={height}
-          width={width}
-        >
+        <Svg height={height} width={width}>
           {this.renderDefs({
             ...config,
             ...this.props.chartConfig
@@ -67,46 +72,48 @@ class BarChart extends AbstractChart {
             height={height}
             rx={borderRadius}
             ry={borderRadius}
-            fill="url(#backgroundGradient)"/>
-            <G>
-          {this.renderHorizontalLines({
-            ...config,
-            count: 4,
-            paddingTop
-          })}</G>
+            fill="url(#backgroundGradient)"
+          />
           <G>
-          {this.renderHorizontalLabels({
-            ...config,
-            count: 4,
-            data: data.datasets[0].data,
-            paddingTop,
-            paddingRight
-          })}
+            {this.renderHorizontalLines({
+              ...config,
+              count: 4,
+              paddingTop
+            })}
           </G>
           <G>
-          {this.renderVerticalLabels({
-            ...config,
-            labels: data.labels,
-            paddingRight,
-            paddingTop,
-            horizontalOffset: barWidth
-          })}
+            {this.renderHorizontalLabels({
+              ...config,
+              count: 4,
+              data: data.datasets[0].data,
+              paddingTop,
+              paddingRight
+            })}
           </G>
           <G>
-          {this.renderBars({
-            ...config,
-            data: data.datasets[0].data,
-            paddingTop,
-            paddingRight
-          })}
+            {this.renderVerticalLabels({
+              ...config,
+              labels: data.labels,
+              paddingRight,
+              paddingTop,
+              horizontalOffset: barWidth
+            })}
           </G>
           <G>
-          {this.renderBarTops({
-            ...config,
-            data: data.datasets[0].data,
-            paddingTop,
-            paddingRight
-          })}
+            {this.renderBars({
+              ...config,
+              data: data.datasets[0].data,
+              paddingTop,
+              paddingRight
+            })}
+          </G>
+          <G>
+            {this.renderBarTops({
+              ...config,
+              data: data.datasets[0].data,
+              paddingTop,
+              paddingRight
+            })}
           </G>
         </Svg>
       </View>
--- src/progress-chart.js
+++ src/progress-chart.js
@@ -1,24 +1,17 @@
 import React from 'react'
-import { View } from 'react-native'
-import {
-  Svg,
-  Text,
-  G,
-  Rect,
-  Path
-} from 'react-native-svg'
+import {View} from 'react-native'
+import {Svg, Text, G, Rect, Path} from 'react-native-svg'
 import AbstractChart from './abstract-chart'
 
 const Pie = require('paths-js/pie')
 
 class ProgressChart extends AbstractChart {
-
   render() {
-    const { width, height, style = {} } = this.props
-    const { borderRadius = 0 } = style
+    const {width, height, style = {}} = this.props
+    const {borderRadius = 0} = style
 
     const pies = this.props.data.map((pieData, i) => {
-      const r = (((height / 2) - 32) / this.props.data.length * i) + 32
+      const r = ((height / 2 - 32) / this.props.data.length) * i + 32
       return Pie({
         r,
         R: r,
@@ -31,7 +24,7 @@ class ProgressChart extends AbstractChart {
     })
 
     const pieBackgrounds = this.props.data.map((pieData, i) => {
-      const r = (((height / 2) - 32) / this.props.data.length * i) + 32
+      const r = ((height / 2 - 32) / this.props.data.length) * i + 32
       return Pie({
         r,
         R: r,
@@ -63,66 +56,75 @@ class ProgressChart extends AbstractChart {
             height={this.props.height}
             rx={borderRadius}
             ry={borderRadius}
-            fill="url(#backgroundGradient)"/>
-          <G
-            x={this.props.width / 2.5}
-            y={this.props.height / 2}
-          >
+            fill="url(#backgroundGradient)"
+          />
+          <G x={this.props.width / 2.5} y={this.props.height / 2}>
             <G>
-            {pieBackgrounds.map(pie => {
-              return (
-                <Path
-                  key={Math.random()}
-                  d={pie.curves[0].sector.path.print()}
-                  strokeWidth={16}
-                  stroke={this.props.chartConfig.color(0.2)}
-                />
-              )
-            })}
+              {pieBackgrounds.map(pie => {
+                return (
+                  <Path
+                    key={Math.random()}
+                    d={pie.curves[0].sector.path.print()}
+                    strokeWidth={16}
+                    stroke={this.props.chartConfig.color(0.2)}
+                  />
+                )
+              })}
             </G>
             <G>
-            {pies.map((pie, i) => {
-              return (
-                <Path
-                  key={Math.random()}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d={pie.curves[0].sector.path.print()}
-                  strokeWidth={16}
-                  stroke={this.props.chartConfig.color((i / pies.length * 0.5) + 0.5)}
-
-                />)
-            })}
+              {pies.map((pie, i) => {
+                return (
+                  <Path
+                    key={Math.random()}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d={pie.curves[0].sector.path.print()}
+                    strokeWidth={16}
+                    stroke={this.props.chartConfig.color(
+                      (i / pies.length) * 0.5 + 0.5
+                    )}
+                  />
+                )
+              })}
             </G>
             <G>
-            {pies.map((_, i) => {
-              return (
-                <Rect
-                  key={Math.random()}
-                  width="16px"
-                  height="16px"
-                  fill={this.props.chartConfig.color(0.2 * (i + 1))}
-                  rx={8}
-                  ry={8}
-                  x={(this.props.width / 2.5) - 24}
-                  y={-(this.props.height / 2.5) + ((this.props.height * 0.8) / this.props.data.length * i) + 12}
-                />
-              )
-            })}
+              {pies.map((_, i) => {
+                return (
+                  <Rect
+                    key={Math.random()}
+                    width="16px"
+                    height="16px"
+                    fill={this.props.chartConfig.color(0.2 * (i + 1))}
+                    rx={8}
+                    ry={8}
+                    x={this.props.width / 2.5 - 24}
+                    y={
+                      -(this.props.height / 2.5) +
+                      ((this.props.height * 0.8) / this.props.data.length) * i +
+                      12
+                    }
+                  />
+                )
+              })}
             </G>
             <G>
-            {pies.map((_, i) => {
-              return (
-                <Text
-                  key={Math.random()}
-                  fill={this.props.chartConfig.color(0.5)}
-                  fontSize="11"
-                  x={this.props.width / 2.5}
-                  y={-(this.props.height / 2.5) + ((this.props.height * 0.8) / this.props.data.length * i) + 12*2}
-                >
-                  {Math.round(100 * this.props.data[i]) + '%'}
-                </Text>)
-            })}
+              {pies.map((_, i) => {
+                return (
+                  <Text
+                    key={Math.random()}
+                    fill={this.props.chartConfig.color(0.5)}
+                    fontSize="11"
+                    x={this.props.width / 2.5}
+                    y={
+                      -(this.props.height / 2.5) +
+                      ((this.props.height * 0.8) / this.props.data.length) * i +
+                      12 * 2
+                    }
+                  >
+                    {Math.round(100 * this.props.data[i]) + '%'}
+                  </Text>
+                )
+              })}
             </G>
           </G>
         </Svg>
