93e363f73f5d3d57506d6d2741cf5f7d19cc4b99
SimonPringleWallace <s.pringlewallace@gmail.com>
2019-12-14 15:29:41 -0500

    [MODIFY] Destructure LineChart Import in App.js
    - Since it is now being exported from an index file, it
      a destructured import.

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React from "react";
 import { ScrollView, StatusBar, Dimensions, Text } from "react-native";
 import ScrollableTabView from "react-native-scrollable-tab-view";
 import FlashMessage, { showMessage } from "react-native-flash-message";
-import LineChart from "./src/line-chart";
+import { LineChart } from "./src/line-chart";
 import PieChart from "./src/pie-chart";
 import ProgressChart from "./src/progress-chart";
 import BarChart from "./src/bar-chart";
