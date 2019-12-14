2183115f0b5d394fd902598cd299567fc4f22bce
SimonPringleWallace <s.pringlewallace@gmail.com>
2019-12-14 16:39:21 -0500

    [ADD] Add Additional Color to Line Chart

--- data.js
+++ data.js
@@ -8,7 +8,8 @@ const data = {
       color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})` // optional
     },
     {
-      data: [20, 10, 4, 56, 87, 90]
+      data: [20, 10, 4, 56, 87, 90],
+      color: (opacity = 1) => `rgba(0, 255, 255, ${opacity})` // optional
     },
     {
       data: [30, 90, 67, 54, 10, 2]
