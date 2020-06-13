251a92aa8c8fd1a77ca6407f8d34100ba11e1a01
Anurag Chutani <anurag002@gmail.com>
2020-06-14 02:22:51 +0530

    Add handling for datasets data is null to use last line coordinates (#347)

--- src/line-chart/line-chart.js
+++ src/line-chart/line-chart.js
@@ -308,7 +308,14 @@ class LineChart extends AbstractChart {
       return this.renderBezierShadow(config);
     }
 
-    const { data, width, height, paddingRight, paddingTop, useColorFromDataset } = config;
+    const {
+      data,
+      width,
+      height,
+      paddingRight,
+      paddingTop,
+      useColorFromDataset
+    } = config;
     const datas = this.getDatas(data);
     const baseHeight = this.calcBaseHeight(datas, height);
     return config.data.map((dataset, index) => {
@@ -332,7 +339,9 @@ class LineChart extends AbstractChart {
                 (dataset.data.length - 1)},${(height / 4) * 3 +
               paddingTop} ${paddingRight},${(height / 4) * 3 + paddingTop}`
           }
-          fill={`url(#fillShadowGradient${useColorFromDataset ? `_${index}` : ''})`}
+          fill={`url(#fillShadowGradient${
+            useColorFromDataset ? `_${index}` : ""
+          })`}
           strokeWidth={0}
         />
       );
@@ -355,13 +364,16 @@ class LineChart extends AbstractChart {
     const output = [];
     const datas = this.getDatas(data);
     const baseHeight = this.calcBaseHeight(datas, height);
+    let lastPoint;
     data.forEach((dataset, index) => {
       const points = dataset.data.map((d, i) => {
+        if (d === null) return lastPoint;
         const x =
           (i * (width - paddingRight)) / dataset.data.length + paddingRight;
         const y =
           ((baseHeight - this.calcHeight(d, datas, height)) / 4) * 3 +
           paddingTop;
+        lastPoint = `${x},${y}`;
         return `${x},${y}`;
       });
       output.push(
@@ -428,7 +440,14 @@ class LineChart extends AbstractChart {
   };
 
   renderBezierShadow = config => {
-    const { width, height, paddingRight, paddingTop, data, useColorFromDataset } = config;
+    const {
+      width,
+      height,
+      paddingRight,
+      paddingTop,
+      data,
+      useColorFromDataset
+    } = config;
     return data.map((dataset, index) => {
       const d =
         this.getBezierLinePoints(dataset, config) +
@@ -440,7 +459,9 @@ class LineChart extends AbstractChart {
         <Path
           key={index}
           d={d}
-          fill={`url(#fillShadowGradient${useColorFromDataset ? `_${index}` : ''})`}
+          fill={`url(#fillShadowGradient${
+            useColorFromDataset ? `_${index}` : ""
+          })`}
           strokeWidth={0}
         />
       );
@@ -486,7 +507,7 @@ class LineChart extends AbstractChart {
       formatXLabel = xLabel => xLabel,
       segments,
       transparent = false,
-      chartConfig = {},
+      chartConfig = {}
     } = this.props;
     const { scrollableDotHorizontalOffset } = this.state;
     const { labels = [] } = data;
@@ -609,7 +630,7 @@ class LineChart extends AbstractChart {
                   data: data.datasets,
                   paddingRight,
                   paddingTop,
-                  useColorFromDataset: chartConfig.useShadowColorFromDataset,
+                  useColorFromDataset: chartConfig.useShadowColorFromDataset
                 })}
             </G>
             <G>
