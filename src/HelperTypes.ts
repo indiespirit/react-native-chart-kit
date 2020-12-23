77c8079715bb9ad1babfa6816ec3f9b15294fb34
James Crain <imcrainjames@gmail.com>
2020-12-23 11:49:01 -0600

    Adds dash support to the LineChart. (#459)

--- src/HelperTypes.ts
+++ src/HelperTypes.ts
@@ -22,6 +22,12 @@ export interface Dataset {
 
   /** Unique key **/
   key?: string | number;
+
+  /** Stroke Dash Array */
+  strokeDashArray?: number[];
+
+  /** Stroke Dash Offset */
+  strokeDashOffset?: number;
 }
 
 export interface ChartData {
